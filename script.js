01ee61f40d3dac90003f69ed6d18b9dac87582a0
awatson32 <audrey.watson32@gmail.com>
2016-02-25 18:54:35 -0600

    Adding onclick function within create element function.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function createButton() {
+function createButton() {   
     var btn = document.createElement("BUTTON");
     var t = document.createTextNode("Click");
     btn.appendChild(t);
     document.body.appendChild(btn);
-    document.getElementByClass('body').addEventListner ("load", createButton);
-}
+      
+};
 
+
+
+//function clickButton() {
+//    onclick = "createButton()";
+//}
